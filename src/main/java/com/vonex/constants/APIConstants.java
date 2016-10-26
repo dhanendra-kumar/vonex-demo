f57132327168a481882d0b3755692d25e6649291
Dhanendra Kumar <dhanendrakumarkht@gmail.com>
2016-10-26 13:48:58 +0530

    Added API Constants.

---
+++ src/main/java/com/vonex/constants/APIConstants.java
@@ -0,0 +1,9 @@
+package com.vonex.constants;
+
+public interface APIConstants {
+
+    String API_HOST_NAME = "https://test.vonex.com.au";
+    String REQUEST_TOKRN_END_POINT = "/api/initial-request";
+    String ASK_END_POINT = "/api/ask";
+    String ANSWER_END_POINT = "/api/answer";
+}
